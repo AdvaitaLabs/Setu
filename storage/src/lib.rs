8665acaddb03d532c2419abbfc71b7959544bbc0
ivan <ivan.coder.zx@gmail.com>
2026-01-08 11:15:11 +0800

    feate: reference setu-merkle

--- storage/src/lib.rs
+++ storage/src/lib.rs
@@ -6,6 +6,7 @@ pub mod object_store;
 // RocksDB storage implementation
 pub mod rocks;
 pub mod rocks_object_store;
+pub mod rocks_merkle_store;
 
 pub use state::*;
 pub use event_store::*;
@@ -15,3 +16,4 @@ pub use object_store::*;
 // Re-export RocksDB types
 pub use rocks::{SetuDB, RocksDBConfig, ColumnFamily, StorageError};
 pub use rocks_object_store::RocksObjectStore;
+pub use rocks_merkle_store::RocksDBMerkleStore;
