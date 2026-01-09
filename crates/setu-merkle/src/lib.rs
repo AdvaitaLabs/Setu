ae825cf7bf69ebb8241ef563c47a4bda3b763709
ivan <ivan.coder.zx@gmail.com>
2026-01-09 18:02:05 +0800

    feat: update export commonly used types

--- crates/setu-merkle/src/lib.rs
+++ crates/setu-merkle/src/lib.rs
@@ -37,7 +37,7 @@ pub mod storage;
 pub use aggregation::{SubnetAggregationProof, SubnetAggregationTree, SubnetStateEntry};
 pub use binary::{BinaryMerkleProof, BinaryMerkleTree};
 pub use error::{MerkleError, MerkleResult};
-pub use hash::HashValue;
+pub use hash::{HashValue, sha256};
 pub use sparse::{IncrementalSparseMerkleTree, SparseMerkleProof, SparseMerkleTree};
 pub use storage::{InMemoryMerkleStore, MerkleNodeStore, MerkleRootStore, MerkleStore};
 
